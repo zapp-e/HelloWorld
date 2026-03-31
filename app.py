b3b2b373667a18fa964bb04c7ff9b9ebe58d7bcf
zapp-e <141183912+zapp-e@users.noreply.github.com>
2026-03-30 18:27:58 -0700

    New reviews

--- app.py
+++ app.py
@@ -12,7 +12,36 @@ print('Hello' + name)
 
 ##Type Conversion
 
+
 birth_year = input('What is your birth year?')
 age = 2026 - int(birth_year)
 
 print(age)
+
+## Strings
+
+course = 'Python for Beginners'
+
+print(course.upper()) ## Makes all uppercase
+
+print(course.find('f'))  ## Find index of the character or word. Case-sensitive
+
+print(course.replace('for', '4'))  # Replaces character or word with new character or word
+
+print('Python' in course)   # Determines if the character or word is in variable
+
+# Arithmetic Operators
+
+print(10+3) #addition
+
+print(10-3) #subtraction
+
+print(10*3) #multiplication
+
+print(10 / 3) #division. Returns float ex. 3.33...
+
+print(10 // 3) #division. Returns integer ex. 3
+
+print(10 % 3) #Returns Remainder
+
+print(10 ** 3) #Exponent operator. 10^3
